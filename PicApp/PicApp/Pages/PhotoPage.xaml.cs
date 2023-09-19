8311bd8ba1a4052008bb1e65bf2ea91e1c54ce3a
boroksid@yandex.ru <boroksid@yandex.ru>
2023-09-19 20:34:52 +0300

    Добавьте файлы проекта.

---
+++ PicApp/PicApp/Pages/PhotoPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PicApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PicApp.Pages
+{
+    public partial class PhotoPage : ContentPage
+    {
+        public PhotoPage(PictureInfo pictureInfo)
+        {
+            InitializeComponent();
+            this.BindingContext = pictureInfo;
+        }
+    }
+}
